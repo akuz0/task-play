3ec0a866a0a191a68ee94824c385bc807c62f04a
alex <alex@bk.ru>
2023-02-27 09:39:41 -0800

    * Changed template

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module homework
 go 1.18
 
 require (
+	github.com/deepmap/oapi-codegen v1.12.4
 	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/google/uuid v1.3.0
--- internal/api/v1/api.go
+++ internal/api/v1/api.go
@@ -2,12 +2,38 @@ package v1
 
 import (
 	"homework/specs"
+	"net/http"
 )
 
 // Быстрая проверка актуальности текущего интерфейса сервера.
 var _ specs.ServerInterface = &apiServer{}
 
 type apiServer struct {
+}
+
+func (a apiServer) AddSong(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a apiServer) NextSong(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a apiServer) PauseList(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a apiServer) PlayList(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a apiServer) PrevSong(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewAPIServer() specs.ServerInterface {
